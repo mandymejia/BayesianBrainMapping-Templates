b5f0d9b8c6aea3e3787cdd34afa304f85c79765a
Nohelia Da Silva <65466801+noheliadasilvas@users.noreply.github.com>
2025-05-08 10:36:35 -0400

    adding template visualization yeo17 and template rds objects

--- src/6_visualization_template.R
+++ src/6_visualization_template.R
@@ -7,6 +7,12 @@ template_files <- list.files(
 for (file in template_files) {
     template <- readRDS(file)
     base_name <- tools::file_path_sans_ext(basename(file))
+
+    # If Yeo17 template, GICA_parc_table needs to be updated to only reflect the correct number of labels (17)
+    if (grepl("yeo17", base_name)) {
+        template$GICA_parc_table <- subset(template$GICA_parc_table, template$GICA_parc_table$Key > 0)
+    }
+
     # Save 4 images for each IC (cortical sd and mean, and subcortical sd and mean)
     plot(template, fname=file.path(dir_data, base_name), idx = 1:dim(template$template$mean)[2])
 }
