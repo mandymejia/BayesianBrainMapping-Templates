0f6ee0a7a3129da8ffa7692b56cd2f04efd5fc83
Nohelia Da Silva <65466801+noheliadasilvas@users.noreply.github.com>
2025-06-19 12:42:32 -0700

    final todos in script

--- src/6_visualization_prior.R
+++ src/6_visualization_prior.R
@@ -1,37 +1,26 @@
 # Plots both the mean and standard deviation components for all priors
 
+prior_files <- list.files(file.path(dir_project, "priors"), recursive = TRUE)
 
-# todo: modify to include the enconding too? 
-
-prior_files <- list.files(
-    path = file.path(dir_data, "priors"),
-    pattern = "^prior_.*\\.rds$", 
-    full.names = TRUE
-  )
-
-get_prior_title <- function(base_name, i, prior) {
-  gsr <- if (grepl("noGSR", base_name)) {
-    "noGSR"
-  } else {
-    "GSR"
-  }
+get_prior_title <- function(base_name, encoding, i, prior) {
+  gsr <- if (grepl("noGSR", base_name)) "noGSR" else "GSR"
 
   if (grepl("Yeo17", base_name, ignore.case = TRUE)) {
-    
     label_name <- rownames(prior$GICA_parc_table)[prior$GICA_parc_table$Key == i]
-
-    return(paste0("Yeo 17 Network ", label_name, " (#", i, ") ", gsr))
+    return(paste0("Yeo 17 Network ", label_name, " (#", i, ") - ", gsr, " (", encoding, ")"))
   }
 
   ic_match <- regmatches(base_name, regexpr("GICA\\d+", base_name))
-  nIC <-  as.numeric(gsub("GICA", "", ic_match))
+  nIC <- as.numeric(gsub("GICA", "", ic_match))
 
-  paste0("GICA ", nIC, gsr, " - Component ", i)
+  paste0("GICA ", nIC, " - Component ", i, " - ", gsr, " (", encoding, ")")
 }
 
 for (file in prior_files) {
     prior <- readRDS(file)
     base_name <- tools::file_path_sans_ext(basename(file))
+    relative_path <- dirname(file)
+    encoding <- strsplit(relative_path, "/")[[1]][1]
 
     # If Yeo17 template, GICA_parc_table needs to be updated to only reflect the correct number of labels (17)
     if (grepl("Yeo17", base_name)) {
@@ -43,16 +32,16 @@ for (file in prior_files) {
     for (i in 1:Q) {
         if (grepl("Yeo17", base_name, ignore.case = TRUE)) {
             label_name <- rownames(prior$GICA_parc_table)[prior$GICA_parc_table$Key == i]
-            fname <- file.path(dir_data, "outputs", "priors_plots", paste0(base_name, "_", label_name))
+            fname <- file.path(dir_data, "outputs", "priors_plots", relative_path, paste0(base_name, "_", label_name))
         } else {
-            fname <- file.path(dir_data, "outputs", "priors_plots", paste0(base_name, "_IC", i))
+            fname <- file.path(dir_data, "outputs", "priors_plots", relative_path, paste0(base_name, "_IC", i))
         }
 
         plot(
             prior,
             fname = fname,
             idx = i,
-            title = get_prior_title(base_name, i, prior)
+            title = get_prior_title(base_name, i, prior, encoding)
         )
     }
 }
--- src/7_visualization_FC.R
+++ src/7_visualization_FC.R
@@ -1,36 +1,29 @@
 # Plots Functional Connectivity (FC) priors for each prior using both the Cholesky and Inverse-Wishart parameterization
 
-# todo: modify to include the enconding too? 
+prior_files <- list.files(file.path(dir_project, "priors"), recursive = TRUE)
 
-prior_files <- list.files(
-    path = file.path(dir_data, "priors"),
-    pattern = "^prior_.*\\.rds$", 
-    full.names = TRUE
-  )
-
-get_prior_title <- function(base_name) {
-  gsr <- if (grepl("noGSR", base_name)) {
-    "noGSR"
-  } else {
-    "GSR"
-  }
+get_prior_title <- function(base_name, encoding) {
+  gsr <- if (grepl("noGSR", base_name)) "noGSR" else "GSR"
 
   if (grepl("Yeo17", base_name, ignore.case = TRUE)) {
-    return(paste0("Yeo17 Prior", gsr))
+    return(paste0("Yeo17 Prior - ", gsr, " (", encoding, ")"))
   }
 
   ic_match <- regmatches(base_name, regexpr("GICA\\d+", base_name))
-  nIC <-  as.numeric(gsub("GICA", "", ic_match))
-  
-  paste0("GICA ", nIC, gsr)
+  nIC <- as.numeric(gsub("GICA", "", ic_match))
+
+  paste0("GICA ", nIC, " - ", gsr, " (", encoding, ")")
 }
 
 for (file in prior_files) {
     prior <- readRDS(file)
     base_name <- tools::file_path_sans_ext(basename(file))
+    relative_path <- dirname(file)
+    encoding <- strsplit(relative_path, "/")[[1]][1]
+
     # Number of ICs
     Q <- dim(prior$template$mean)[2]
-    plot_title <- get_prior_title(base_name)
+    plot_title <- get_prior_title(base_name, encoding)
     
     # FC Cholesky 
     prior_FC_mean_Cholesky <- prior$template$FC_Chol$FC_samp_mean
@@ -39,17 +32,17 @@ for (file in prior_files) {
     diag(prior_FC_mean_Cholesky) <- diag(prior_FC_var_Cholesky) <- NA
 
     # Save as PDF (2 pages)
-    pdf(file.path(dir_data, "outputs", paste0(base_name, "_FC_Cholesky.pdf")), height=5, width=5.5)
-    plot_FC(prior_FC_mean_Cholesky, zlim=zlim_FC, title = paste0(plot_title, "\nCholesky FC Prior Mean")) # nolint
-    plot_FC(sqrt(prior_FC_var_Cholesky), zlim=c(0.1, 0.3), cols = viridis(12), title = paste0(plot_title, "\nCholesky FC Prior SD")) # nolint
+    pdf(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_Cholesky.pdf")), height=5, width=5.5)
+    plot_FC(prior_FC_mean_Cholesky, zlim=zlim_FC, title = paste0(plot_title, "\nCholesky FC Prior Mean"))
+    plot_FC(sqrt(prior_FC_var_Cholesky), zlim=c(0.1, 0.3), cols = viridis(12), title = paste0(plot_title, "\nCholesky FC Prior SD"))
     dev.off()
 
     # Save as png (2 images)
-    png(file.path(dir_data, "outputs", paste0(base_name, "_FC_Cholesky_mean.png")), height = 550, width = 600)
+    png(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_Cholesky_mean.png")), height = 550, width = 600)
     plot_FC(prior_FC_mean_Cholesky, zlim = zlim_FC, title = paste0(plot_title, "\nCholesky FC Prior Mean"))
     dev.off()
 
-    png(file.path(dir_data, "outputs", paste0(base_name, "_FC_Cholesky_sd.png")), height = 550, width = 600)
+    png(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_Cholesky_sd.png")), height = 550, width = 600)
     plot_FC(sqrt(prior_FC_var_Cholesky), zlim = c(0.1, 0.3), cols = viridis(12), title = paste0(plot_title, "\nCholesky FC Prior SD"))
     dev.off()
 
@@ -64,17 +57,17 @@ for (file in prior_files) {
     diag(prior_FC_mean_InverseWishart) <- diag(prior_FC_var_InverseWishart) <- NA
 
     # Save as PDF (2 pages)
-    pdf(file.path(dir_data, "outputs", paste0(base_name, "_FC_InverseWishart.pdf")), height=5, width=5.5)
+    pdf(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_InverseWishart.pdf")), height=5, width=5.5)
     plot_FC(prior_FC_mean_InverseWishart, zlim=zlim_FC, title = paste0(plot_title, "\nInverse-Wishart FC Prior Mean"))
     plot_FC(sqrt(prior_FC_var_InverseWishart), zlim=c(0.1, 0.3), cols = viridis(12), title = paste0(plot_title, "\nInverse-Wishart FC Prior SD"))
     dev.off()
 
     # Save as png (2 images)
-    png(file.path(dir_data, "outputs", paste0(base_name, "_FC_InverseWishart_mean.png")), height = 550, width = 600)
+    png(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_InverseWishart_mean.png")), height = 550, width = 600)
     plot_FC(prior_FC_mean_InverseWishart, zlim = zlim_FC, title = paste0(plot_title, "\nInverse-Wishart FC Prior Mean"))
     dev.off()
 
-    png(file.path(dir_data, "outputs", paste0(base_name, "_FC_InverseWishart_sd.png")), height = 550, width = 600)
+    png(file.path(dir_data, "outputs", "priors_plots", relative_path, "FC", paste0(base_name, "_FC_InverseWishart_sd.png")), height = 550, width = 600)
     plot_FC(sqrt(prior_FC_var_InverseWishart), zlim = c(0.1, 0.3), cols = viridis(12), title = paste0(plot_title, "\nInverse-Wishart FC Prior SD"))
     dev.off()
 }
